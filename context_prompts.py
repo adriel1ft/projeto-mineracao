66589d8ab6a25da3034bacec6a508150b4a6ea52
Kamily Assis <kamilyfacul@gmail.com>
2025-04-14 20:54:11 -0300

    fix v1

--- context_prompts.py
+++ context_prompts.py
@@ -1,4 +1,4 @@
-format_prompt_q2 = r"""The input I give you and the output I expect
+context_q2 = r"""The input I give you and the output I expect
 are DFM schemata in YAML formatted as follows: (1)
 the fact is a “fact” tag including a “name” tag; (2) all
 measures are listed inside a “measures” tag, each is an
@@ -13,36 +13,7 @@ fact to each measure; (5) all descriptive attributes, if
 any, are listed under a “descriptive” tag; (6) all optional
 attributes, if any, are listed under an “optional” tag
 
-
-----
-INPUT YAML FILE: {content}
-MSG : {msg}
-
-"""
-
-format_prompt_q3 = r"""The input I give you and the output I expect
-are DFM schemata in YAML formatted as follows: (1)
-the fact is a “fact” tag including a “name” tag; (2) all
-measures are listed inside a “measures” tag, each is an
-empty item containing a “name” tag; (3) all many-to-
-one associations between attributes in a hierarchy are
-listed inside a “dependencies” tag: each is an empty
-item containing a “from” tag, listing the finer attribute,
-a “to” tag, listing the coarser attribute, and optionally
-a “role” tag; (4) the “dependencies” list also includes an
-item from the fact to each dimension, and one from the
-fact to each measure; (5) all descriptive attributes, if
-any, are listed under a “descriptive” tag; (6) all optional
-attributes, if any, are listed under an “optional” tag
-
-
-----
-INPUT YAML FILE: {content}
-MSG : {msg}
-
-"""
-
-task_prompt_q2 = """ You receive in input a draft DFM
+You receive in input a draft DFM
 schema obtained by supply-driven design. A DFM 
 schema is a connected graph where the fact is a node
 in which no arcs enter. [...] Your task is to refine the
@@ -56,10 +27,35 @@ is semi-additive, just leave its name as it is. (3) Find
 descriptive attributes. (3bis) Discretize attributes. (4)
 Find optional attributes. (5) Complete time hierarchies. 
 (6) Remove uninteresting attributes.
+
+----
+INPUT YAML FILE: {content}
+MSG : {msg}
+
 """
 
-procedure_prompt_q3 = """(2) Classify measures into additive, semi-additive, or non-additive. If you find a measure is non-additive, i.e., it can never be summed (e.g., the unit price of a product, a currency exchange, or a discount percentage), you should add “(AVG)” to its name.
+context_q3 = r"""The input I give you and the output I expect
+are DFM schemata in YAML formatted as follows: (1)
+the fact is a “fact” tag including a “name” tag; (2) all
+measures are listed inside a “measures” tag, each is an
+empty item containing a “name” tag; (3) all many-to-
+one associations between attributes in a hierarchy are
+listed inside a “dependencies” tag: each is an empty
+item containing a “from” tag, listing the finer attribute,
+a “to” tag, listing the coarser attribute, and optionally
+a “role” tag; (4) the “dependencies” list also includes an
+item from the fact to each dimension, and one from the
+fact to each measure; (5) all descriptive attributes, if
+any, are listed under a “descriptive” tag; (6) all optional
+attributes, if any, are listed under an “optional” tag
+
+(2) Classify measures into additive, semi-additive, or non-additive. If you find a measure is non-additive, i.e., it can never be summed (e.g., the unit price of a product, a currency exchange, or a discount percentage), you should add “(AVG)” to its name.
 If you find it is semi-additive, i.e., that it can be summed along all hierarchies except temporal ones (e.g., inventory level), you should add “(SUM-AVG)” to its name.
 If you find it is additive, i.e., it can be summed along all hierarchies (e.g., the number of products sold in a day and the monthly revenue), just leave its name as it is.
 Make sure you rename measures also under the “dependencies” tag.
+
+----
+INPUT YAML FILE: {content}
+MSG : {msg}
+
 """
